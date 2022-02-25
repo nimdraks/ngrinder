bd96e30ef23764532a7c777b0e8a417f657c4aea
nimdrak <nimdrak@naver.com>
2022-02-25 03:40:10 -0500

    TTL 10

--- ngrinder-runtime/src/main/java/org/ngrinder/dns/NameStore.java
+++ ngrinder-runtime/src/main/java/org/ngrinder/dns/NameStore.java
@@ -64,7 +64,7 @@ public class NameStore {
 			singleton = new NameStore();
 			singleton.initFromSystemProperty();
 			try {
-				java.security.Security.setProperty("networkaddress.cache.ttl", "30");
+				java.security.Security.setProperty("networkaddress.cache.ttl", "10");
 			} catch (Exception e) {
 				noOp();
 			}
--- ngrinder-runtime/src/main/java/org/ngrinder/sm/NGrinderSecurityManager.java
+++ ngrinder-runtime/src/main/java/org/ngrinder/sm/NGrinderSecurityManager.java
@@ -154,7 +154,7 @@ public class NGrinderSecurityManager extends SecurityManager {
 		// add controller host
 		allowedHost.add(consoleIP);
 		try {
-			java.security.Security.setProperty("networkaddress.cache.ttl", "30");
+			java.security.Security.setProperty("networkaddress.cache.ttl", "10");
 		} catch (Exception e) {
 			// Fall through
 		}
